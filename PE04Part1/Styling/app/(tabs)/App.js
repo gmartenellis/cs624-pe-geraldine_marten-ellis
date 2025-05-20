bc7b67bad2cd818d8b0f490758be5802599d4822
Geraldine Marten-Ellis <germartenellis@gmail.com>
2025-05-20 16:36:43 +0000

    PE04Part1-Styling

--- PE04Part1/Styling/app/(tabs)/App.js
+++ PE04Part1/Styling/app/(tabs)/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Image, StyleSheet, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native' 
 
 
 export default class App extends Component {
@@ -10,6 +10,23 @@ export default class App extends Component {
                     <View style={styles.cardImageContainer}>
                         <Image style={styles.cardImage}
                             source={require('../../assets/images/icons8-user-50.png')}/>
+                    </View>
+                    <View>
+                        <Text style={styles.cardName}>
+                            John Doe
+                        </Text>
+                    </View>
+                    <View style={styles.cardOccupationContainer}>
+                        <Text style={styles.cardOccupation}>
+                            React Native Developer
+                        </Text>
+                    </View>
+                    <View>
+                        <Text style={styles.cardDescription}>
+                            John is a really great JavaScript developer. He
+                            loves using JS to build React Native applications
+                            for iOS and Android.
+                        </Text>
                     </View>
                 </View>
             </View>
@@ -49,5 +66,33 @@ const styles = StyleSheet.create({
     cardImage: {
         width: 80,
         height: 80
+    },
+    cardName: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 24,
+        marginTop: 30,
+        textShadowColor: 'black',
+        textShadowOffset: {
+            height: 2,
+            width: 2
+        },
+        textShadowRadius: 3
+    },
+    cardOccupationContainer: {
+        borderColor: 'black',
+        borderBottomWidth: 3
+    },
+    cardOccupation: {
+        fontWeight: 'bold',
+        marginTop: 10,
+        marginBottom: 10,
+    },
+    cardDescription: {
+        fontStyle: 'italic',
+        marginTop: 10,
+        marginRight: 40,
+        marginLeft: 40,
+        marginBottom: 10
     }
 });
